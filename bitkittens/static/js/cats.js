349d299b35e4096f33942dc556707a40e7551d70
Stanley Sham <stanley.sham94@gmail.com>
2019-09-08 17:38:46 -0400

    completed assignment

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,2 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const summonButton = document.querySelector('.summon-cats');
+    const newCats = axios.get('https://eric-deploy-test.herokuapp.com/cats'); //Using different link since bitkitten is down
+    summonButton.addEventListener('click', function() {
+        console.log('testing hi there');
+        let catPicker = Math.floor(Math.random() * 3);
+        let boxPicker = Math.floor(Math.random() * 3) + 1;
+        newCats.then( (response) => {
+            let catBox = document.querySelector(`#cat${boxPicker}`);
+            let catPic = response.data['cats'][catPicker]['photo'];
+            let newCat = document.createElement('img');
+            newCat.src = catPic;
+            catBox.appendChild(newCat);
+        });
+    });
+
+
 });
